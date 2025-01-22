170fab8b29b48e1fe46277412b08340ff018b14a
Lukasz Frankowski <lukasz.frankowski@zondaglobal.com>
2025-01-22 22:30:25 +0100

    python tutorial

---
+++ 01-basics/01-03-primitives.py
@@ -0,0 +1,43 @@
+#######################################################################################################################
+print("int and float")
+def int_and_float():
+    print(type(1))  # <class 'int'>
+    print(type(1.1))  # <class 'float'>
+    print(5/2, type(5/2))  # 2.5 float (floating point integer division)
+    print(5//2, type(5//2), 5%2, type(5%2))  # 2 int 1 int (integer division and modulo)
+                                             # funny, complaints about no spaces around modulo operator, but not about
+                                             # division operators
+int_and_float()
+
+#######################################################################################################################
+print("\nstrings")
+def strings():
+
+    # quotes (no defined in code style)
+    print('hello single quoted')  # hello single quoted
+    print("hello double quoted")  # hello double-quoted
+
+    # special chars
+    print("hello\nnew line")  # hello
+                              # new line
+    print(r"hello\nraw string")  # hello\nraw string
+
+    # multiline
+    print("""hello
+    new line""")  # hello
+                  #           newline
+                  # so \n at the end of each line is preserved
+    print("""hello\
+    same line""")  # hello    same line
+                  # to skip \n use "\" at the end of line
+                  # but sill, "new line" is indented as in the code, this can be overcome with textwrap.dedent()
+    print("hello "
+          "same line")  # hello same line
+                        # better multiline strings - auto concatenation with no operator
+    print("hello " +
+          "same line")  # hello same line
+                        # the same with "+" operator
+
+    # multiplication
+    print(3 * "hello ")  # hello hello hello
+strings()
