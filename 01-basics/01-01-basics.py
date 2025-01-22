d70c71014f2141185b6a750239c3a7b38b97e7b1
Lukasz Frankowski <lukasz.frankowski@zondaglobal.com>
2025-01-22 22:02:27 +0100

    python tutorial

---
+++ 01-basics/01-01-basics.py
@@ -0,0 +1,46 @@
+#######################################################################################################################
+print("no local code block, and can't be easily worked around")
+if True:
+    localScopeVar = 10  # variable names convention looks inconsistent, here they don't complain I used camel case
+print(localScopeVar)  # 10
+# conclusion: it looks as a truly misleading inconsistency in relation to other langs because not always
+# a code block creates a scope; sometimes it creates and sometimes not
+# only function, class, etc. creates a local scope, but it's a function, class, etc...
+def local_scope_test():
+    local_scope_var = 10  # here they complaints about camel case and I have to use snake case
+# print(local_scope_var)  # can't: local_scope_var' is not defined
+
+#######################################################################################################################
+print("\nshadowing vars")
+global_scope_var = 1
+def shadow_vars_1():
+    print(global_scope_var)
+shadow_vars_1()  # 1
+def shadow_vars_2():
+    # print(global_scope_var)  # this is even not possible, if I declare local var it shadows global_scope_var
+                               # and because it's declared AFTER this invocation it's not visible for this line
+    # <----- so here's a block where global_scope_var does not exist at all
+    global_scope_var = 2
+    print(global_scope_var)
+shadow_vars_2()  # 2
+print(global_scope_var)  # 1
+# conclusion: outer scope var can be shadowed, a new variable is defined in this scenario and outer scope var is
+# left untouched
+
+#######################################################################################################################
+print("\ndynamic typing - a base flaw like in other scripting languages")
+def dynamic_typing():
+    spam = 1
+    print(type(spam))  # <class 'int'>
+    spam = "1"
+    print(type(spam))  # <class 'str'>
+dynamic_typing()
+
+#######################################################################################################################
+print("\nbut they now that and worked around already so Python TypeScript unlikely appears")
+def type_hints():
+    spam: int = 1
+    print(type(spam))  # <class 'int'>
+    spam = "1"         # now here it complains with warning, but still works (look at the next line)
+    print(type(spam))  # <class 'int'>
+type_hints()
