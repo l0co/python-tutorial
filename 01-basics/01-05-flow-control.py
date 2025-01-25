0fccc32fee9ee688c3d01c5467cfc81fc59a698e
Lukasz Frankowski <lukasz.frankowski@zondaglobal.com>
2025-01-25 19:58:52 +0100

    python tutorial

--- 01-basics/01-05-flow-control.py
+++ 01-basics/01-05-flow-control.py
@@ -1,6 +1,6 @@
 #######################################################################################################################
 print("standard if")
-def standard_if(x: int):  # for all those who are scared about "dynamic typing" examples, type hints DOES work for
+def standard_if(x: int):  # for all those who are scared about "dynamic typing" examples, type hints DO work for
                           # functions so you might try to write a consistent code in Python as well
     if x == 1:
         print("one")
@@ -16,3 +16,14 @@ def standard_if(x: int):  # for all those who are scared about "dynamic typing" 
 standard_if(1)  # STDOUT: one
 standard_if(2)  # STDOUT: two
 standard_if(3)  # STDOUT: more than two
+
+#######################################################################################################################
+print("standard while")
+def standard_while(x: int):
+    while x < 10:
+        print("inc; ", end="")  # end argument is an end line char, default is "\n" but one can overwrite
+        x = x + 1
+    else:
+        print("x reached 10")
+standard_while(5)  # STDOUT: inc; inc; inc; inc; inc; x reached 10
+
