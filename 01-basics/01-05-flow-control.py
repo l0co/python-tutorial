8d7a6de9113ce06234fd8b7d1b5b9f08b9e52ee3
Lukasz Frankowski <lukasz.frankowski@zondaglobal.com>
2025-01-27 17:12:47 +0100

    python tutorial

--- 01-basics/01-05-flow-control.py
+++ 01-basics/01-05-flow-control.py
@@ -38,6 +38,30 @@ def for_in():
     print("")
     for x in range(0, 9, 3):  # in range with custom step (3)
         print(x, end="")  # STDOUT: 036
+    print("")
+    print(range(0, 2).__iter__())  # STDOUT: <range_iterator object at 0x7f5f5053b900>
+                                   # is range a list? not - all arguments passed to "for" should implement
+                                   # "interable" pattern, but in Python's way:
+    from collections.abc import Iterable
+    print(isinstance(range(0, 2), Iterable))  # STDOUT: True
+    print(isinstance([0, 1, 2], Iterable))  # STDOUT: True
 for_in()
 
+#######################################################################################################################
+print("\nstandard break and continue")
+def break_continue():
+    for x in range(1, 100):  # A-loop
+        print("x=", x, end=" ", sep="")
+        for y in range(1, 100):  # B-loop
+            print("y=", y, end=" ", sep="")
+            break  # breaks B-loop
+        if x % 2 == 0:
+            break  # breaks A-loop
+    print("")
+    # but no label-based break, though
+    for x in range(1, 10):
+        if x % 2 == 0:
+            continue
+        print("x=", x, end=" ", sep="")
+break_continue()  # STDOUT: x=1 x=3 x=5 x=7 x=9
 
