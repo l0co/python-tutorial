7bac6d680c95343ff54509da92c3938e953725dd
Lukasz Frankowski <lukasz.frankowski@zondaglobal.com>
2025-02-13 19:12:52 +0100

    python tutorial

--- 01-basics/08-flow-control.py
+++ 01-basics/08-flow-control.py
@@ -1,7 +1,7 @@
 #######################################################################################################################
 print("standard 'if'")
-def standard_if(x: int):  # for all those who are scared about "dynamic typing" examples, type hints DO work for
-                          # functions so you might try to write a consistent code in Python as well
+def standard_if(x: int):  # for all those who are scared about "dynamic typing" examples, type hints do work for
+                          # functions so you might try to write a consistent code in Python as well (might try)
     if x == 1:
         print("one")
     elif x == 2:
@@ -9,7 +9,7 @@ def standard_if(x: int):  # for all those who are scared about "dynamic typing" 
     else:
         print("more than two")
 # standard_if()  # ERROR: standard_if() missing 1 required positional argument
-                 # leaving this example to point out that this is an INTERPRETED language
+                 # leaving this example to point out that this is an interpreted language
                  # this means that they go each line one after another and try to execute it
                  # if there's an error, you'll only know that in the runtime
                  # so, calling a function with one argument, with no arguments at all is "permitted"
