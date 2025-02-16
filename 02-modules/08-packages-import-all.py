4ddc39522560c15a849b2cd1147004b2b786de1b
Lukasz Frankowski <lukasz.frankowski@zondaglobal.com>
2025-02-16 11:53:02 +0100

    python tutorial

--- 02-modules/08-packages-import-all.py
+++ 02-modules/08-packages-import-all.py
@@ -1,5 +1,3 @@
-from mypackage.mysubpackage.mymodule import *
-
 # init __init__.py (see mypackage/mysubpackage/__init__.py) one can define which files are imported by default with *
 # wildcard: in this scenario we import mymodule, but not mymodule2
 
